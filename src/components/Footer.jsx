865b72ebb1f03d8a5a46e72e18bc3ef638ed4406
nasgmr <noviaazkasalsabila@gmail.com>
2026-05-29 16:12:21 +0700

    fix: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,9 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-left">
-        <p className="copyright-text" font-size="12px">Copyright &copy; 2026 AMX UAV</p>
+        <p className="copyright-text">Copyright &copy; 2026 AMX UAV</p>
         <p className="powered-text">Powered by AMX UAV</p>
       </div>
-
       <div className="footer-right">
         <p>Wirosaban Barat No.208 Yogyakarta, Indonesia</p>
         <p>+62 811292565</p>
